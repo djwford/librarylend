cf981cfabe317ac667e844f5be48c5ae0c16aa4c
Derek <derekweatherford@gmail.com>
2013-11-30 13:06:27 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Librarylend::Application.config.secret_token = '41379cd56e287955114043dfb70f92c3e3c8c3a5f9879f75cf8993e852d1065ef54e05db44a950dd47032e147ef330457ea27b5a4b950c7c214d1aea5ef7a51d'
